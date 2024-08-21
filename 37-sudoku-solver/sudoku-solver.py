d06fd66552cb4b408ef12dbab5686fe641d36e78
Anurag Singh <anurag.dhanju@gmail.com>
2024-08-21 11:24:16 -0400

    Time: 480 ms (43.40%) | Memory: 11.8 MB (22.04%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.py
+++ 37-sudoku-solver/sudoku-solver.py
@@ -3,30 +3,41 @@ class Solution(object):
         """
         :type board: List[List[str]]
         :rtype: None Do not return anything, modify board in-place instead.
-        """
-        def validGrid(row, col, num):
+        """        
+        def validGrid(row, col, ch):
+            row, col = int(row), int(col)
+            
             for i in range(9):
-                if board[row][i] == str(num) or board[i][col] == str(num):
+                
+                if board[i][col] == ch:
                     return False
-            row_start = (row // 3) * 3
-            col_start = (col // 3) * 3
-            for i in range(3):
-                for j in range(3):
-                    if board[row_start + i][col_start + j] == str(num):
-                        return False
+                if board[row][i] == ch:
+                    return False
+                
+                if board[3*(row//3) + i//3][3*(col//3) + i%3] == ch:
+                    return False
+            
             return True
+            
+        def solveGrid(row, col):
+            if row == 9:
+                return True
+            if col == 9:
+                return solveGrid(row+1, 0)
+            
+            if board[row][col] == ".":
+                for i in range(1, 10):
+                    if validGrid(row, col, str(i)):
+                        board[row][col] = str(i)
+                        
+                        if solveGrid(row, col + 1):
+                            return True
+                        else:
+                            board[row][col] = "."
+                return False
+            else:
+                return solveGrid(row, col + 1)
+            
+            
         
-        def solveGrid(board):
-            for i in range(9):
-                for j in range(9):
-                    if board[i][j] == ".":
-                        for num in range(1, 10):
-                            if validGrid(i, j, num):
-                                board[i][j] = str(num)
-                                if solveGrid(board):
-                                    return True
-                                board[i][j] = "."
-                        return False
-            return True
-        
-        solveGrid(board)
+        solveGrid(0, 0)
