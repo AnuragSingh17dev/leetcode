af79ca752880e713a3d49bad2405ce4403c23512
Anurag Singh <anurag.dhanju@gmail.com>
2024-08-21 11:17:03 -0400

    Time: 484 ms (42.17%) | Memory: 11.9 MB (22.04%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def solveSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """        
+        def validGrid(row, col, ch):
+            row, col = int(row), int(col)
+            
+            for i in range(9):
+                
+                if board[i][col] == ch:
+                    return False
+                if board[row][i] == ch:
+                    return False
+                
+                if board[3*(row//3) + i//3][3*(col//3) + i%3] == ch:
+                    return False
+            
+            return True
+            
+        def solveGrid(row, col):
+            if row == 9:
+                return True
+            if col == 9:
+                return solveGrid(row+1, 0)
+            
+            if board[row][col] == ".":
+                for i in range(1, 10):
+                    if validGrid(row, col, str(i)):
+                        board[row][col] = str(i)
+                        
+                        if solveGrid(row, col + 1):
+                            return True
+                        else:
+                            board[row][col] = "."
+                return False
+            else:
+                return solveGrid(row, col + 1)
+            
+            
+        
+        solveGrid(0, 0)
