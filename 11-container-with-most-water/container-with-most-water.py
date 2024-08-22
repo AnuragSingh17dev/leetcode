3ad9dbb60b92a48cb945c3413f4453d7371b903d
Anurag Singh <anurag.dhanju@gmail.com>
2024-08-22 16:10:49 -0400

    Time: 475 ms (97.31%) | Memory: 22 MB (83.42%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -4,16 +4,13 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        lp = 0
-        rp = len(height) - 1
-        maxCapacity = 0
-        while lp < rp:
-            width = rp - lp
-            h = min(height[lp], height[rp])
-            capacity = width * h
-            maxCapacity = max(maxCapacity, capacity)
-            if height[lp] < height[rp]:
+        maxCapacity, lp, rp = 0, 0, len(height)-1
+        while (lp < rp):
+            if height[lp] <= height[rp]:
+                capacity = height[lp] * (rp - lp)
                 lp += 1
             else:
+                capacity = height[rp] * (rp - lp)
                 rp -= 1
+            if capacity > maxCapacity: maxCapacity = capacity
         return maxCapacity
