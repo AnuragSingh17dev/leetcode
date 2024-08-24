6d6d6338c16cdd5fd113dadda70bf7ee90c9d408
Anurag Singh <anurag.dhanju@gmail.com>
2024-08-24 12:47:27 -0400

    Time: 12 ms (81.68%) | Memory: 11.9 MB (7.40%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -11,7 +11,7 @@ class Solution(object):
             value = x % 10
             num = num * 10 + value
             x /= 10
-        result = signed * num
-        if result > 2 ** 31 - 1 or result < -(2 ** 31):
+        output = signed * num
+        if output > 2 ** 31 - 1 or output < -(2 ** 31):
             return 0
-        return result
+        return output
