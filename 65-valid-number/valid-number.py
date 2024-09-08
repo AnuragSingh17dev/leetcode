86b68795feef5226dbc4c2314d63eab06a8c4e7f
Anurag Singh <anurag.dhanju@gmail.com>
2024-09-08 16:44:24 -0400

    Time: 30 ms (5.06%) | Memory: 11.6 MB (63.91%) - LeetSync

---
+++ 65-valid-number/valid-number.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def isNumber(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        num, exp, sign, dec = False, False, False, False
+        for c in s:
+            if c >= '0' and c <= '9':
+                num = True     
+            elif c == 'e' or c == 'E':
+                if exp or not num: 
+                    return False
+                else: 
+                    exp, num, sign, dec = True, False, False, False
+            elif c == '+' or c == '-':
+                if sign or num or dec: 
+                    return False
+                else: 
+                    sign = True
+            elif c == '.':
+                if dec or exp: 
+                    return False
+                else: 
+                    dec = True
+            else: 
+                return False
+        return num
