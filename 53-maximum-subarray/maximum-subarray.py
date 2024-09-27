0285606da0ce6538017b7b1bcec1aa4264a7f966
Anurag Singh <anurag.dhanju@gmail.com>
2024-09-27 18:26:58 -0400

    Time: 555 ms (40.54%) | Memory: 24.6 MB (46.31%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -6,10 +6,8 @@ class Solution(object):
         """
         if not nums:
             return 0
-
         curSum = maxSum = nums[0]
         for num in nums[1:]:
             curSum = max(num, curSum + num)
             maxSum = max(maxSum, curSum)
-
         return maxSum
